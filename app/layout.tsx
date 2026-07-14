50ee90a2b86d04df22f3c9ffad1282186e7b9662
Abu Nasir Limon <leemon174@gmail.com>
2026-07-14 14:52:43 +0600

    feat(analytics): add Microsoft Clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { SiteFooter } from "@/components/layout/site-footer";
 import { SiteHeader } from "@/components/layout/site-header";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -117,6 +118,16 @@ export default function RootLayout({
           <SiteFooter />
         </CartProvider>
         <GoogleAnalytics gaId="G-TFT6V5ZRHH" />
+
+<Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xm8offk239");
+  `}
+</Script>
       </body>
     </html>
   );
